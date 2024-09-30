53a3d150536d99f1117d0b66e0fb8b89ca868e2e
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-09-30 17:12:55 -0300

    feat: config react router dom project

--- about-me/src/App.js
+++ about-me/src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import  Inicio from '../src/paginas/Inicio'
+import SobreMin from '../src/paginas/SobreMin'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />}/>
+        <Route path="/sobreMin" element={<SobreMin />}/>
+        <Route path="*" element={<div>Page Not found</div>}/>
+      </Routes>
+    </BrowserRouter>
   );
+
+  
 }
 
 export default App;
---
+++ about-me/src/paginas/SobreMin/index.js
@@ -0,0 +1,5 @@
+export default function SobreMin(){
+    return (
+        <h1>About Me</h1>
+    )
+}
